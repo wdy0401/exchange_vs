3cb5ea7ec69c19990f475c1afbfa8f1f6c662fd4
wdy0401 <wdy0401@gmail.com>
2014-09-02 14:14:34 +0800

    test git
    20140902 qt protobuffer zip utf82gbk
    
    20140902 qt protobuffer zip
    utf82gbk

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include"mainwindow.h"
+#include<QApplication>
+#include<map>
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+
+#include"./protobuf/stdafx.h"
+#include"./protobuf/quote.pb.h"
+#include<Windows.h>
+#include<string>
+#include<list>
+#include<stdio.h>
+
+#pragma comment(lib,"libprotobuf.lib")
+#pragma comment(lib,"libprotoc.lib")
+
+
+
+#include"gpp_qt/cfg/cfg.h"
+#include"gpp_qt/wtimer/wtimer.h"
+#include"gpp_qt/wfunction/wfunction.h"
+
+#include"gpp_qt/log_info/log_info.h"
+
+
+#include"gpp_qt/bar/bar.h"
+#include"gpp_qt/datafeed/datafeed.h"
+#include"gpp_qt/trans_event/trans_event.h"
+#include"gpp_qt/fillpolicy/fillpolicy.h"
+
+#include"gpp_qt/match_engine/match_engine.h"
+#include"gpp_qt/match_engine/order.h"
+#include"gpp_qt/match_engine/orderlist.h"
+#include"gpp_qt/match_engine/orderbook.h"
+
+#include"test_function.h"
+
+using namespace std;
+using namespace QUOTE;
+
+
+
+cfg simucfg;
+log_info loginfo;
+match_engine me;
+datafeed df;
+trans_event te;
+wtimer tm;
+
+MainWindow * mw;
+QApplication * qa;
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    qa=&a;
+    mw=&w;
+
+    w.show();
+
+    //runtactic();
+	//test_protobuf();
+	//test_protobuf_order();
+	TestStream();
+    return a.exec();
+}
+
+//test git
