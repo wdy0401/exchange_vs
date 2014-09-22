86c9777e0454c8d7f9022e569a347d8a43be3207
wdy0401 <wdy0401@gmail.com>
2014-09-22 14:45:45 +0800

    tactic logic in ctp
    tactic inheriance from ctp and add feather to support vs_exchange

--- main.cpp
+++ main.cpp
@@ -1,44 +1,42 @@
 #include"mainwindow.h"
 #include<QApplication>
 #include<map>
-#include<iostream>
+#include<list>
+#include<string>
 #include<fstream>
+#include<iostream>
+
+#include<stdio.h>
 #include<stdlib.h> 
+#include<Windows.h>
 
 #include"./protobuf/stdafx.h"
 #include"./protobuf/quote.pb.h"
-#include<Windows.h>
-#include<string>
-#include<list>
-#include<stdio.h>
-
-#pragma comment(lib,"libprotobuf.lib")
-#pragma comment(lib,"libprotoc.lib")
 
 #include"gpp_qt/cfg/cfg.h"
 #include"gpp_qt/wtimer/wtimer.h"
-#include"gpp_qt/wfunction/wfunction.h"
-
 #include"gpp_qt/log_info/log_info.h"
-
+#include"gpp_qt/wfunction/wfunction.h"
 
 #include"gpp_qt/bar/bar.h"
 #include"gpp_qt/datafeed/datafeed.h"
-#include"gpp_qt/trans_event/trans_event.h"
 #include"gpp_qt/fillpolicy/fillpolicy.h"
+#include"gpp_qt/trans_event/trans_event.h"
 
-#include"gpp_qt/match_engine/match_engine.h"
 #include"gpp_qt/match_engine/order.h"
 #include"gpp_qt/match_engine/orderlist.h"
 #include"gpp_qt/match_engine/orderbook.h"
+#include"gpp_qt/match_engine/match_engine.h"
 
 #include"test_function.h"
 
 #include"tactic/tactic/tactic.h"
 
+#pragma comment(lib,"libprotobuf.lib")
+#pragma comment(lib,"libprotoc.lib")
+
 using namespace std;
 using namespace QUOTE;
-
 
 
 cfg simucfg;
@@ -62,7 +60,7 @@ int main(int argc, char *argv[])
 
     w.show();
 	
-	
+	//20140922 猜测是用lib给定的信息在默认目录找的
 	//string dllname;
 	//#ifdef _DEBUG
 	//dllname="C:/Users/xzqh/Desktop/exchange/exchange_vs/tactic/Debug/tactic.dll";
